4e9c35b947335e8cc32eef441b7592bebfa9872b
adnan0758 <21131a4456@gvpce.ac.in>
2024-01-14 10:37:06 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,7 @@
+//<div id="adnan">
+//<div id='school'
+//h1>flora</h1>
+const heading = React.createElement("div", {id:"adnan"},React.createElement("div",{id:"school"},[React.createElement("h1",{},"flora"),React.createElement("h2",{},"vuyyur")]),
+React.createElement("div", {id:"adnan"},React.createElement("div",{id:"clg"},[React.createElement("h1",{},"flora"),React.createElement("h2",{},"vuyyur")])));
+console.log(heading);
+        ReactDOM.render(heading,document.getElementById("foot"));
